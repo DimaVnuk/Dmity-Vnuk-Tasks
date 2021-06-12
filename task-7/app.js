bad6e0d6f2697d2ee952c85bd9956ae0c0513e0f
DimaVnuk <vnuchok311994@hotmail.com>
2021-06-12 18:37:27 +0300

    added script

--- task-7/app.js
+++ task-7/app.js
@@ -1,1 +1,31 @@
 'use strict'
+
+
+const obj = {
+    a: 5,
+    b:{
+        c:5
+    }
+}
+
+
+const copyObj = obj
+
+const deepCopyObj = deepClone(obj)
+
+
+function deepClone(obj){
+    const cloneObj ={}
+    for(const prop in obj){
+        if(obj[i] instanceof Object) {
+            cloneObj[i] = deepClone([obj[i]])
+            continue
+        }
+        cloneObj[i] = obj[i]
+    }
+    return cloneObj
+}
+
+deepCopyObj.addnlProp = { fd: 45 };
+console.log('Z object after cloning: ', obj);
+console.log('Y object: ', deepCopyObj);
