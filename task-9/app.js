e0a7f4e36527f1ca19c8c0e082c19b0503efc2a1
DimaVnuk <vnuchok311994@hotmail.com>
2021-07-02 23:41:04 +0300

    feature: added array of months

--- task-9/app.js
+++ task-9/app.js
@@ -1,1 +1,20 @@
 'use strict'
+const log = console.log
+const date = new Date();
+
+const months = [
+'January',
+'February',
+'March',
+'April',
+'May',
+'June',
+'July',
+'August',
+'September',
+'October',
+'November',
+'December',
+]
+
+
