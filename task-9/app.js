079722844b8a16e352d19bb39235bfc02202ebc2
DimaVnuk <vnuchok311994@hotmail.com>
2021-07-07 02:22:41 +0300

    feature: added inactive days

--- task-9/app.js
+++ task-9/app.js
@@ -1,25 +1,63 @@
+let nav = 0;
+let clicked = null;
+let events = localStorage.getItem("events")
+  ? JSON.parse(localStorage.getItem("events"))
+  : [];
+
+const newEventModal = document.getElementById("newEventModal");
+const backDrop = document.getElementById("modalBackDrop");
+const monthDays = document.querySelector(".days");
+const weekDays = document.querySelector(".weekdays");
+const eventTitle = document.getElementById("eventTitleInput");
+const startWeek = document.getElementById("startWithMon");
+const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const dayOfWeekStartWithMon = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 const date = new Date();
+const day = date.getDate();
+const month = date.getMonth();
+const year = date.getFullYear();
+
+function openModal(dt) {
+  clicked = dt;
+
+  const eventForDay = events.find((e) => e.dt === clicked);
+
+  if (eventForDay) {
+    document.getElementById("eventText").innerText = eventForDay.title;
+    deleteEventModal.style.display = "block";
+  } else {
+    newEventModal.style.display = "block";
+  }
+  backDrop.style.display = "block";
+}
+
+function renderCalendar() {
+  date.setDate(1);
 
 
-const renderCalendar = () => {
-  date.setDate(1)
-  
-  
-  
-  const monthDays = document.querySelector('.days')
-  
-  const lastDay = new Date(date.getFullYear(),date.getMonth()+1, 0).getDate()
-  
-  const prevLastDay = new Date(date.getFullYear(),date.getMonth(), 0).getDate()
-  
-  
-  const firstDayIndex = date.getDay()
-  
-  const lastDayIndex = new Date(date.getFullYear(),date.getMonth()+1, 0).getDay()
-  
-  const nextDays = 7- lastDayIndex - 1
 
-  
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate(); //daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
   const months = [
     "January",
     "February",
@@ -34,65 +72,40 @@ const renderCalendar = () => {
     "November",
     "December",
   ];
-  
-  
-  document.querySelector('.date h1').innerHTML = months[date.getMonth()]; // get current months
-  
-  document.querySelector('.date p').innerHTML = new Date().toDateString(); // get current day, months, day, year
-  
-  
-  let days = '';
-  
-  for(let x = firstDayIndex; x > 0 ;x--){ // prev days
-   days+=`<div class="prev-date">${prevLastDay-x + 1}</div>`
-  };
-  
-  
-  
-  for(let i = 1; i <= lastDay; i++){ // current day
-    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
-      days += `<div class = "today">${i}</div>`
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()]; // get current months
+
+  document.querySelector(".date p").innerHTML = new Date().toDateString(); // get current day, months, day, year
+
+  let week = "";
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    // prev days
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let k = 0; k < dayOfWeek.length; k++) {
+    week += `<div>${dayOfWeek[k]}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    // current day
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class = "today">${i}</div>`;
     } else {
-  days += `<div>${i}</div>`
-  /* monthDays.innerHTML = days */
+      days += `<div class = 'click-div'>${i}</div>`;
     }
   }
-  
-  
-  for(let j = 1; j <= nextDays;j++){ // prev days
-    days += `<div class="next-date">${j}</div>`
-   
-   };
-   monthDays.innerHTML = days
-   
+
+  for (let j = 1; j <= nextDays; j++) {
+    // prev days
+    days += `<div class="next-date">${j}</div>`;
+  }
+  monthDays.innerHTML = days;
+  weekDays.innerHTML = week;
 }
-
-
-
-
-
-document.querySelector('.prev').addEventListener('click', () =>{
-date.setMonth(date.getMonth()-1)
-renderCalendar()
-})
-
-
-document.querySelector('.next').addEventListener('click', () =>{
-  date.setMonth(date.getMonth()+1)
-  renderCalendar()
-})
-
-
-
-renderCalendar()
-
-
-
-
-
-
-
-
-
-
 
--- task-9/config.js
+++ task-9/config.js
@@ -1,10 +1,84 @@
-configWindow.hidden = true
-config.addEventListener("click", () => {
-    
-  configWindow.hidden = false
-});
+const prevDay = document.getElementsByClassName("prev-date");
+const nextDay = document.getElementsByClassName("next-date");
 
-iconClose.addEventListener("click", () => {
+function inactivePrevDays() {
+  hideInactiveDays.addEventListener("click", () => {
+    for (let i = 0; i < prevDay.length; i++) {
+      prevDay[i].style.opacity = "0";
+    }
+    for (let j = 0; j < nextDay.length; j++) {
+      nextDay[j].style.opacity = "0";
+    }
+  });
+  showInactiveDays.addEventListener("click", () => {
+    for (let i = 0; i < prevDay.length; i++) {
+      prevDay[i].style.opacity = "0.5";
+    }
+    for (let j = 0; j < nextDay.length; j++) {
+      nextDay[j].style.opacity = "0.5";
+    }
+  });
+}
+
+monthDays.addEventListener("click", () =>
+  openModal(`${month + 1}/ ${day}/ ${year}`)
+);
+
+
+function initButtons() {
+    document.querySelector(".prev").addEventListener("click", () => {
+      date.setMonth(date.getMonth() - 1);
+      renderCalendar();
+    });
   
-  configWindow.hidden = true
-});
+    document.querySelector(".next").addEventListener("click", () => {
+      date.setMonth(date.getMonth() + 1);
+      renderCalendar();
+    });
+  }
+  
+  
+  
+function closeModal() {
+    newEventModal.style.display = "none";
+    backDrop.style.display = "none";
+    eventTitleInput.value = "";
+    clicked = null;
+    renderCalendar();
+  }
+  
+  
+function saveEvent() {
+    if (eventTitleInput.value) {
+      eventTitleInput.classList.remove("error");
+  
+      events.push({
+        date: clicked,
+        title: eventTitleInput.value,
+      });
+  
+      localStorage.setItem("events", JSON.stringify(events));
+      closeModal();
+    } else {
+      eventTitleInput.classList.add("error");
+    }
+  }
+  
+function configOpen() {
+    configWindow.hidden = true;
+    config.addEventListener("click", () => {
+      configWindow.hidden = false;
+    });
+  
+    iconClose.addEventListener("click", () => {
+      configWindow.hidden = true;
+    });
+  
+    document.getElementById("saveButton").addEventListener("click", saveEvent);
+    document.getElementById("cancelButton").addEventListener("click", closeModal);
+  }
+
+inactivePrevDays();
+configOpen();
+initButtons();
+renderCalendar();
